dc6995aa78bb1f312bd254e4c47a053eb988eb90
HaphazardLegoman <50499591+HaphazardLegoman@users.noreply.github.com>
2025-07-27 11:35:23 -0500

    Update Open-Bitmap.ps1
    added alternate version of Open-Bitmap without a validation workload to compare performance

--- Private/Open-Bitmap.ps1
+++ Private/Open-Bitmap.ps1
@@ -40,6 +40,67 @@ function Open-Bitmap {
     }
 }
 
+Using namespace System.Diagnostics.CodeAnalysis
+Using namespace System.Drawing
+Using namespace System.IO
+Using namespace System.Management.Automation
+
+function Open-BitmapAlt {
+#TODO : test performance across these variants
+    [CmdletBinding()] [OutputType([Bitmap])]
+
+    [SuppressMessageAttribute('PSReviewUnusedParameter', '', Target = 'FilePath', Justification = 'This parameter is being used in a manner or scope that PSScriptAnalyzer cannot see.')]
+
+    Param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateScript({
+                Try {
+                    $givenPath = $_
+                    $resolvedPath = Resolve-Path -Path $givenPath -ErrorAction Stop
+                    If (-not (Get-Item -Path $_ | Select-Object -ExpandProperty PSIsContainer)) { $true } Else {
+                        Throw [ValidationMetadataException]::new("`"$givenPath`" is not a file.")
+                    } 
+                } Catch [ItemNotFoundException] {
+                    Throw [ItemNotFoundException]::new("The file `"$givenPath`" does not exist.", $_.Exception)
+                }
+            })]
+        [String[]]$FilePath
+    )
+
+    Process {
+        Foreach ($f in $FilePath) {
+            Try { [Bitmap]::FromFile($f) } Catch {
+                Write-Error [ValidationMetadataException]::new("`"$f`" could not be opened as a bitmap object.", $_.Exception)
+            } 
+        }
+    }
+}
+
+function Test-ValidateScript {
+
+    [CmdletBinding()] [OutputType([String])]
+
+    [SuppressMessageAttribute('PSReviewUnusedParameter', '', Target = 'FilePath', Justification = 'This parameter is being used in a manner or scope that PSScriptAnalyzer cannot see.')]
+
+    Param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateScript({
+                $i = $_
+                If ($_ -like 'a*') {
+                    Set-Variable -PassThru -Scope 1 -Name "valString$([io.path]::GetRandomFileName())" -Value "${_}_validated" | ForEach-Object {[Boolean]$_}
+                } Else {
+                    Throw "I don't like $i"
+                }
+            })]
+        [String[]]$InputString
+    )
+
+    Process { Get-Variable -Name 'valString*' | ForEach-Object {
+        $_.Value
+        Remove-Variable -Name $_.Name
+    } }
+}
+
 function Test-ValidateScript {
 
     [CmdletBinding()] [OutputType([String])]
